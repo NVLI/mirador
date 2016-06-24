62245e68d34748b48c2830934c05222d2a15df6d
Safallia Joseph <safallia.joseph@zyxware.com>
2016-06-24 12:51:49 +0530

    NVLI-42 Attach mirador function inside Drupal behaviours Check whether the viewer is already loaded, in the page as Drupal.attachBehaviors() may be called more times under different circumstances after the DOM is loaded.

--- js/mirador-load.js
+++ js/mirador-load.js
@@ -1,17 +1,25 @@
 (function ($, Drupal) {
-  var manifestUri = drupalSettings.init.entity.manifest_uri;
-  var viewerID = drupalSettings.init.entity.viewer_id;
-  Mirador({
-    "id": viewerID,
-    "layout": "1x1",
-    'showAddFromURLBox' : false,
-    "saveSession": false,
-    "data": [
-      { "manifestUri": manifestUri, "location": "National Virtual Library Of India"},
-    ],
-    "windowObjects":[{
-      "loadedManifest" : manifestUri,
-      "viewType" : "ImageView"} ],
-  });
+
+  Drupal.behaviors.Mirador = {
+    attach: function (context, settings) {
+      var manifestUri = settings.init.entity.manifest_uri;
+      var viewerID = settings.init.entity.viewer_id;
+      if ($('#' + viewerID + ' .mirador-viewer').length == 0) {
+        Mirador({
+          "id": viewerID,
+          "layout": "1x1",
+          'openManifestsPage' : false,
+          'showAddFromURLBox' : false,
+          "saveSession": false,
+          "data": [
+            { "manifestUri": manifestUri, "location": "National Virtual Library Of India"},
+          ],
+          "windowObjects":[{
+            "loadedManifest" : manifestUri,
+            "viewType" : "ImageView"} ],
+        });
+      }
+    }
+  };
 })(jQuery, Drupal);
 
