79df4d5109998b3f8e7c7fdd4b149058a6e2e92c
Safallia Joseph <safallia.joseph@zyxware.com>
2016-06-30 19:57:43 +0530

    NVLI-70 Extend mirador endpoint.js to store annotations in drupal
    Add a new js file js/miradorEndPoint.js for storing annotation in drupal

--- js/mirador-load.js
+++ js/mirador-load.js
@@ -8,7 +8,10 @@
   Drupal.behaviors.Mirador = {
     attach: function (context, settings) {
       var manifestUri = settings.init.entity.manifest_uri;
+      var annotationUri = settings.init.entity.annotation_uri;
       var viewerID = settings.init.entity.viewer_id;
+      var imageRefEntityID = settings.init.entity.entity_id;
+      var userID = settings.init.entity.user_id;
       if ($('#' + viewerID + ' .mirador-viewer').length == 0) {
         Mirador({
           "id": viewerID,
@@ -22,9 +25,20 @@
           "windowObjects":[{
             "loadedManifest" : manifestUri,
             "viewType" : "ImageView"} ],
+            /** Annotations **/
+            annotationEndpoint: {
+              name: 'Mirador Endpoint',
+              module: 'MiradorEndpoint',
+              options: {
+                url: annotationUri,
+                  storeId: 'demo.nvli',
+                  APIKey: 'user_auth',
+                  imageRefEntityID: imageRefEntityID,
+                  annotationOwner: userID
+              }
+            }
         });
       }
     }
   };
 })(jQuery, Drupal);
-
