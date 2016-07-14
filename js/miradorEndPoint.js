5d2457bba9f3c994c1c92e2083e6115c63f931a0
safallia_zyxware <safallia_zyxware@2974359.no-reply.drupal.org>
2016-07-14 10:15:47 +0530

    Issue #2764727 by safallia.joseph, vimaljoseph, praveenmr19791: Create annotation using mirador viewer

---
+++ js/miradorEndPoint.js
@@ -0,0 +1,269 @@
+/**
+ * @file
+ * Drupal Endpoint for mirador annoations.
+ *
+ * All Endpoints need to have at least the following:
+ * annotationsList - current list of OA Annotations
+ * dfd - Deferred Object
+ * init()
+ * search(uri)
+ * create(oaAnnotation, returnSuccess, returnError)
+ * update(oaAnnotation, returnSuccess, returnError)
+ * deleteAnnotation(annotationID, returnSuccess, returnError) (delete is a reserved word)
+ * TODO:
+ * There is a bug in that if you create an annotation and then delete it (without moving pages) then click either the write annotation button
+ * or try to create a new annotation the deleted annotation re-appears.
+ * Changing pages fixes the issue as the annoation is delete from the annotation store.
+ */
+
+(function($){
+
+  $.MiradorEndpoint = function(options) {
+    // If user does not have permission to annotate, display a message instead
+      // of add annotation link.
+    jQuery.extend(this, {
+      token:     null,
+      uri:      null,
+      url:      options.url,
+      imageRefEntity: options.imageRefEntityID,
+      annotationOwner: options.annotationOwner,
+      annotationSettings: options.annotationSettings,
+      xcrfToken: options.xcrfToken,
+      dfd:       null,
+      annotationsList: [],
+      // Internal list for module use to map id to URI.
+      idMapper: {}
+    }, options);
+
+    this.init();
+  };
+
+  $.MiradorEndpoint.prototype = {
+    // Any set up for this endpoint, and triggers a search of the URI passed to object.
+    init: function() {
+      this.catchOptions = {
+        user: {
+          id: this.userid,
+          name: this.username
+        },
+        permissions: {
+          'read':   [],
+          'update': [this.userid],
+          'delete': [this.userid],
+          'admin':  [this.userid]
+        }
+      };
+      this.search({ uri: this.uri });
+    },
+
+    // Search endpoint for all annotations with a given URI.
+    search: function(options, successCallback, errorCallback) {
+      var _this = this;
+      annotationSettings = jQuery.parseJSON(_this.annotationSettings);
+      // Clear out current list.
+      this.annotationsList = [];
+      jQuery.ajax({
+        url: annotationSettings.annotation_search_uri,
+        type: annotationSettings.annotation_search_method,
+        dataType: 'json',
+        headers: {
+        },
+        data: {
+          uri: annotationSettings.annotation_search_uri,
+          imageRefEntityID: options.imageRefEntityID,
+          media: "image",
+          limit: 10000
+        },
+
+        contentType: "application/json; charset=utf-8",
+        success: function(data) {
+          if (typeof successCallback === "function") {
+            successCallback(data);
+          }
+          else {
+            _this.annotationsList = data;
+            jQuery.each(_this.annotationsList, function(index, value) {
+              value.fullId = value["@id"];
+              value["@id"] = $.genUUID();
+              _this.idMapper[value["@id"]] = value.fullId;
+              value.endpoint = _this;
+            });
+            _this.dfd.resolve(false);
+          }
+        },
+        error: function() {
+          if (typeof errorCallback === "function") {
+            errorCallback();
+          }
+          else {
+            console.log("The request for annotations has caused an error for endpoint: " + options.uri);
+          }
+        }
+      });
+    },
+
+    deleteAnnotation: function(annotationID, returnSuccess, returnError) {
+      var _this = this;
+      annotationSettings = jQuery.parseJSON(_this.annotationSettings);
+      var xcrfToken = _this.xcrfToken;
+      var drupal_annotation_id = this.idMapper[annotationID];
+      var annotationDeleteUri = annotationSettings.annotation_delete_uri;
+      annotationDeleteUri = annotationDeleteUri.replace("{annotation_id}", drupal_annotation_id);
+      jQuery.ajax({
+        url: annotationDeleteUri,
+        type: annotationSettings.annotation_delete_method,
+        headers: {
+          "Content-Type": 'application/hal+json',
+          "X-CSRF-Token": xcrfToken,
+        },
+        contentType: "*",
+        success: function(data) {
+          returnSuccess();
+        },
+        error: function() {
+          returnError();
+        }
+
+      });
+    },
+
+    update: function(oaAnnotation, returnSuccess, returnError) {
+      var annotation = oaAnnotation,
+          _this = this;
+      shortId = annotation["@id"];
+      var xcrfToken = _this.xcrfToken;
+      annotation["@id"] = annotation.fullId;
+      annotationID = annotation.fullId;
+      annotationSettings = jQuery.parseJSON(_this.annotationSettings);
+      var annotationUpdateUri = annotationSettings.annotation_update_uri;
+      annotationUpdateUri = annotationUpdateUri.replace("{annotation_id}", annotationID);
+      var drupalAnnotationStore = {};
+      // Generate annotation data to be stored as Drupal entity.
+      if (annotationSettings.endpoint == "rest_endpoint") {
+        drupalAnnotationStore['_links'] = {};
+        drupalAnnotationStore['_links']['type'] = {};
+        drupalAnnotationStore['_links']['type']['href'] = annotationSettings.type_url;
+        drupalAnnotationStore[annotationSettings.annotation_text] = {};
+        drupalAnnotationStore[annotationSettings.annotation_text]['value'] = annotation['resource']['0']['chars'];
+        var annotation_data = JSON.stringify(drupalAnnotationStore);
+      }
+      else {
+        drupalAnnotationStore['uri'] = {};
+        drupalAnnotationStore['uri'] = annotation['on']['source'];
+        drupalAnnotationStore['text'] = {};
+        drupalAnnotationStore['text'] = annotation['resource']['0']['chars'];
+        drupalAnnotationStore['data'] = {};
+        drupalAnnotationStore['data'] = annotation;
+        drupalAnnotationStore['type'] = 'image annotation';
+        var annotation_data = JSON.stringify(drupalAnnotationStore);
+      }
+      jQuery.ajax({
+        url: annotationUpdateUri,
+        type: annotationSettings.annotation_update_method,
+        dataType: 'json',
+        headers: {
+          "Content-Type": 'application/hal+json',
+          "X-CSRF-Token": xcrfToken,
+        },
+        data: annotation_data,
+        contentType: "application/json; charset=utf-8",
+        success: function(data) {
+          // This returned data doesn't seem to be used anywhere.
+          returnSuccess();
+        },
+        error: function() {
+          returnError();
+        }
+      });
+      // This is what updates the viewer.
+      annotation.endpoint = _this;
+      annotation.fullId = annotation["@id"];
+      annotation["@id"] = shortId;
+    },
+
+    create: function(oaAnnotation, returnSuccess, returnError) {
+      var annotation = oaAnnotation,
+          _this = this;
+      var xcrfToken = _this.xcrfToken;
+      annotation['imgRefEntity'] = _this.imageRefEntityID;
+      annotation['annotationOwner'] = this.annotationOwner;
+      annotationSettings = jQuery.parseJSON(_this.annotationSettings);
+      var drupalAnnotationStore = {};
+      if (annotationSettings.endpoint == "rest_endpoint") {
+        // Generate annotation data to be stored as Drupal entity.
+        drupalAnnotationStore['_links'] = {};
+        drupalAnnotationStore['_links']['type'] = {};
+        drupalAnnotationStore['_links']['type']['href'] = annotationSettings.type_url;
+        if (annotationSettings.annotation_text != "title") {
+          drupalAnnotationStore['title'] = {};
+          drupalAnnotationStore['title']['value'] = annotation['resource']['0']['chars'];
+        }
+        drupalAnnotationStore[annotationSettings.annotation_text] = {};
+        drupalAnnotationStore[annotationSettings.annotation_text]['value'] = annotation['resource']['0']['chars'];
+        drupalAnnotationStore[annotationSettings.annotation_image_entity] = {};
+        drupalAnnotationStore[annotationSettings.annotation_image_entity]['und'] = {};
+        drupalAnnotationStore[annotationSettings.annotation_image_entity]['und']['target_id'] = annotation['imgRefEntity'];
+        drupalAnnotationStore[annotationSettings.annotation_viewport] = {};
+        drupalAnnotationStore[annotationSettings.annotation_viewport]['value'] = annotation['on']['selector']['value'];
+        drupalAnnotationStore[annotationSettings.annotation_resource] = {};
+        drupalAnnotationStore[annotationSettings.annotation_resource]['value'] = annotation['on']['source'];
+        drupalAnnotationStore['type'] = {};
+        drupalAnnotationStore['type']['target_id'] = annotationSettings.annotation_entity_bundle;
+        var annotation_data = JSON.stringify(drupalAnnotationStore);
+      }
+      else {
+        drupalAnnotationStore['uri'] = {};
+        drupalAnnotationStore['uri'] = annotation['on']['source'];
+        drupalAnnotationStore['text'] = {};
+        drupalAnnotationStore['text'] = annotation['resource']['0']['chars'];
+        drupalAnnotationStore['data'] = {};
+        drupalAnnotationStore['data'] = annotation;
+        drupalAnnotationStore['type'] = 'image annotation';
+        var annotation_data = JSON.stringify(drupalAnnotationStore);
+      }
+      jQuery.ajax({
+        url: annotationSettings.annotation_create_uri,
+        type: annotationSettings.annotation_create_method,
+        dataType: 'json',
+        headers: {
+          "Content-Type": 'application/hal+json',
+          "X-CSRF-Token": xcrfToken,
+        },
+        data: annotation_data,
+        contentType: "application/json; charset=utf-8",
+        success: function(result, status, xhr) {
+          var annotation_id = result.id;
+          if (annotationSettings.endpoint == "rest_endpoint") {
+            if (result['nid']['0']['value'] != 'undefined') {
+              annotation_id = result['nid']['0']['value'];
+            }
+          }
+          annotation['id'] = annotation_id;
+          annotation['@id'] = annotation_id;
+          annotation['fullId'] = annotation_id;
+          data = JSON.stringify(annotation);
+          data.fullId = annotation_id;
+          data["@id"] = $.genUUID();
+          data.endpoint = _this;
+          _this.idMapper[data["@id"]] = annotation_id;
+          returnSuccess(data);
+        },
+        error: function() {
+          returnError();
+        }
+      });
+    },
+
+    set: function(prop, value, options) {
+      if (options) {
+        this[options.parent][prop] = value;
+      }
+      else {
+        this[prop] = value;
+      }
+    },
+    userAuthorize: function(action, annotation) {
+      return true;
+    }
+  };
+}(Mirador));
---
+++ mirador_annotation/src/Controller/MiradorAnnotationController.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\mirador_annotation\Controller\MiradorAnnotationController.
+ */
+
+namespace Drupal\mirador_annotation\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Controller routines for manifest creation.
+ */
+class MiradorAnnotationController extends ControllerBase {
+
+  /**
+   * Page Callback: Search Annotation.
+   */
+  public function searchAnnotation($image_entity_id) {
+    $output = array();
+    $output = $this->getAnnotation($image_entity_id);
+    return new JsonResponse($output);
+  }
+
+  /**
+   * Returns the annotations list.
+   */
+  public function getAnnotation($image_id) {
+    $annotations = array();
+    // Load the mirador global settings.
+    $config = \Drupal::config('mirador.settings');
+    // Annotation settings.
+    $annotation_entity = $config->get('annotation_entity');
+    $annotation_text = $config->get('annotation_text');
+    $annotation_viewport = $config->get('annotation_viewport');
+    $annotation_image_entity = $config->get('annotation_image_entity');
+    $annotation_image_resource = $config->get('annotation_resource');
+
+    // Fetch the annotation entity_ids.
+    $query = \Drupal::entityQuery($annotation_entity)
+      ->condition("$annotation_image_entity.entity.nid", $image_id);
+    $annotation_ids = $query->execute();
+
+    // Load the annotations.
+    $annotation_data = entity_load_multiple($annotation_entity, $annotation_ids);
+
+    $options = array('absolute' => TRUE);
+    foreach ($annotation_data as $annotation) {
+      $node_url = \Drupal\Core\Url::fromRoute("entity.$annotation_entity.canonical", ["$annotation_entity" => $annotation->nid->value], $options);
+      $node_url = $node_url->toString();
+      $resource[] = array(
+        '@type' => 'dctypes:Text',
+        'id' => $annotation->nid->value,
+        'format' => 'text/html',
+        'chars' => $annotation->$annotation_text->value,
+      );
+      $on = array(
+        '@id' => $annotation->nid->value,
+        '@type' => 'oa:SpecificResource',
+        'selector' => array(
+          '@id' => $annotation->nid->value,
+          '@type' => 'oa:FragmentSelector',
+          'value' => $annotation->$annotation_viewport->value,
+        ),
+        "full" => $annotation->$annotation_image_resource->value,
+      );
+      $annotations[] = array(
+        '@id' => $annotation->nid->value,
+        '@type' => 'oa:Annotation',
+        'motivation' => array('oa:commenting'),
+        'resource' => $resource,
+        'on' => $on,
+      );
+    }
+    return $annotations;
+  }
+
+}
